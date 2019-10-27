85ba568440b1c884692a4bca0888a56b506ba2bc
jomamma417 <johnnyl@cpp.edu>
2019-10-27 15:27:30 -0700

    Update schema with default admin status to FALSE

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:pos, :user_status]
   # before_action :authenticate_admin, only: [:managerment]
   def home
+    if(current_user.admin?)
+      flash[:info] = "Logged in as admin"
+    end
   end
 
   def about
