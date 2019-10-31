2120c00a2d6d5d909c99fc9f53c74e4c86c3e46b
Brandon Gastelo <bagastelo@cpp.edu>
2019-10-30 19:07:38 -0700

    Add pagination to search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,14 +5,22 @@ class SearchController < ApplicationController
     if params.has_key?(:search)
       terms = search_params
       unless terms[:isbn].blank?
-        @results = Book.where(isbn: terms[:isbn]).limit(RESULTS_CONST)
+        @results = Book.where(isbn: terms[:isbn])
       else
-        @results = Book.all.limit(RESULTS_CONST)
+        @results = Book.all
         @results = @results.where("title LIKE ?", "%#{terms[:title]}%") unless terms[:title].blank?
         @results = @results.where("author LIKE ?", "%#{terms[:author]}%") unless terms[:author].blank?
       end
     else
-      @results = Book.all.limit(RESULTS_CONST)
+      @results = Book.all
+    end
+    @results_count = @results.count
+    @pages = @results_count / RESULTS_CONST + 1
+    @page = (params[:page] || 1).to_i
+    @results = @results.limit(RESULTS_CONST).offset(RESULTS_CONST * (@page - 1))
+    @links = Array.new(@pages - 1)
+    @pages.times do |i|
+      @links[i] = search_path(params.merge(page: i + 1).permit({search: [:title, :author]}, :page))
     end
   end
 
