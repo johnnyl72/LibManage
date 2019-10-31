63e8db2c4bf22b3c20d96a3f350b4210afc95a1b
Brandon Gastelo <bagastelo@cpp.edu>
2019-10-31 10:19:30 -0700

    Add search by ISBN

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
     if params.has_key?(:search)
       terms = search_params
       unless terms[:isbn].blank?
-        @results = Book.where(isbn: terms[:isbn])
+        @results = Book.where(isbn: terms[:isbn].strip)
       else
         @results = Book.all
         @results = @results.where("title LIKE ?", "%#{terms[:title]}%") unless terms[:title].blank?
