4be54bb110b061d3686613a5f9e2496ba4f3e766
Brandon Gastelo <bagastelo@cpp.edu>
2019-11-04 12:35:55 -0800

    Add buttons to Book page to manage book items

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,7 @@ class BooksController < ApplicationController
   # GET /books/1.json
   def show
     @book_items = @book.book_items
+    @is_admin = current_user && current_user.admin?
   end
 
   # GET /books/new
