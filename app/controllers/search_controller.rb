dbf651883600f7fec900f24e68d469872769541c
Brandon Gastelo <bagastelo@cpp.edu>
2019-11-13 14:34:49 -0800

    Add images to search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,11 @@
 class SearchController < ApplicationController
-  RESULTS_CONST = 10
+  RESULTS_CONST = 10 # Number of results to display per page
 
   def search
+    # Check if search variable present
     if params.has_key?(:search)
       terms = search_params
+      # Check if searching by query or by ISBN
       unless terms[:isbn].blank?
         @results = Book.where(isbn: terms[:isbn].strip)
       else
@@ -14,14 +16,21 @@ class SearchController < ApplicationController
     else
       @results = Book.all
     end
+
+    # Pagination
     @results_count = @results.count
     @pages = @results_count / RESULTS_CONST + 1
     @page = (params[:page] || 1).to_i
+    # Select subset of results using page number
     @results = @results.limit(RESULTS_CONST).offset(RESULTS_CONST * (@page - 1))
+    # Compute links to different pages
     @links = Array.new(@pages - 1)
     @pages.times do |i|
       @links[i] = search_path(params.merge(page: i + 1).permit({search: [:title, :author]}, :page))
     end
+
+    # Optimize for image query
+    @results = @results.with_attached_image
   end
 
   private
